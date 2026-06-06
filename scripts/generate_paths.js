303bcd047308ba0b1ce1bfaed75b3ae7c8105039
Kyler <178511452+anoner9000@users.noreply.github.com>
2026-06-05 22:19:56 -0500

    Exclude private VRMs from generated paths

--- scripts/generate_paths.js
+++ scripts/generate_paths.js
@@ -4,18 +4,46 @@ const path = require('path');
 
 const ROOT = path.join(__dirname, './../public');
 
-const bgImages = fg.globSync(ROOT+"/bg/**/bg-*.jpg", {dot: true}).map((p) => p.split(ROOT)[1]);
-const vrmList = fg.globSync(ROOT+"/vrm/**/*.vrm", {dot: true}).map((p) => p.split(ROOT)[1]);
-const speechT5SpeakerEmbeddingsList = fg.globSync(ROOT+"/speecht5_speaker_embeddings/**/*.bin", {dot: true}).map((p) => p.split(ROOT)[1]);
-const animationList = [].concat(
-  fg.globSync(ROOT+"/animations/**/*.vrma", {dot: true}).map((p) => p.split(ROOT)[1]),
-  fg.globSync(ROOT+"/animations/**/*.fbx", {dot: true}).map((p) => p.split(ROOT)[1])
-);
+const toPublicPath = (root, filePath) => filePath.split(root)[1];
 
-let str = "";
-str += `export const bgImages = ${JSON.stringify(bgImages)};\n`;
-str += `export const vrmList = ${JSON.stringify(vrmList)};\n`;
-str += `export const speechT5SpeakerEmbeddingsList = ${JSON.stringify(speechT5SpeakerEmbeddingsList)};\n`;
-str += `export const animationList = ${JSON.stringify(animationList)};\n`;
+const buildPaths = (root = ROOT) => {
+  const bgImages = fg.globSync(root+"/bg/**/bg-*.jpg", {dot: true}).map((p) => toPublicPath(root, p));
+  const vrmList = fg.globSync(root+"/vrm/**/*.vrm", {dot: true})
+    .map((p) => toPublicPath(root, p))
+    .filter((p) => !p.startsWith("/vrm/.private/"));
+  const speechT5SpeakerEmbeddingsList = fg.globSync(root+"/speecht5_speaker_embeddings/**/*.bin", {dot: true}).map((p) => toPublicPath(root, p));
+  const animationList = [].concat(
+    fg.globSync(root+"/animations/**/*.vrma", {dot: true}).map((p) => toPublicPath(root, p)),
+    fg.globSync(root+"/animations/**/*.fbx", {dot: true}).map((p) => toPublicPath(root, p))
+  );
 
-fs.writeFileSync(path.join(__dirname, './../src/paths.ts'), str);
+  return {
+    bgImages,
+    vrmList,
+    speechT5SpeakerEmbeddingsList,
+    animationList,
+  };
+};
+
+const renderPaths = ({bgImages, vrmList, speechT5SpeakerEmbeddingsList, animationList}) => {
+  let str = "";
+  str += `export const bgImages = ${JSON.stringify(bgImages)};\n`;
+  str += `export const vrmList = ${JSON.stringify(vrmList)};\n`;
+  str += `export const speechT5SpeakerEmbeddingsList = ${JSON.stringify(speechT5SpeakerEmbeddingsList)};\n`;
+  str += `export const animationList = ${JSON.stringify(animationList)};\n`;
+  return str;
+};
+
+const writePaths = () => {
+  fs.writeFileSync(path.join(__dirname, './../src/paths.ts'), renderPaths(buildPaths()));
+};
+
+if (require.main === module) {
+  writePaths();
+}
+
+module.exports = {
+  buildPaths,
+  renderPaths,
+  writePaths,
+};
